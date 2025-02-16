82c288b0cf61fdf2ef8e5864739c8b28bb63dbf7
santavalleytea <takayaueno0712@gmail.com>
2025-02-16 16:42:00 +1100

    Added encryption

--- src/client.c
+++ src/client.c
@@ -3,10 +3,56 @@
 #include <string.h> // memset() and string ops
 #include <arpa/inet.h> // sockaddr_in and inet_pton()
 #include <unistd.h> // close()
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/dh.h>
 
 #define BUFFER_SIZE 1024
+#define AES_KEY_SIZE 32
+#define AES_BLOCK_SIZE 16
+
+int aes_encrypt (const unsigned char *plaintext, unsigned char *ciphertext, int plaintext_len, unsigned char *key, unsigned char *iv) {
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int ciphertext_len;
+
+    // Create encryption context
+    if (!(ctx = EVP_CIPHER_CTX_new())) {
+        perror("EVP_CIPHER_CTX_new Failed\n.");
+        return -1;
+    }
+
+    // Initialize AES Encryption
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv) != 1) {
+        perror("EVP_EncryptInit_ex failed");
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    // Data encryption
+    if (EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len) != 1) {
+        perror("EVP_EncryptUpdate failed");
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    ciphertext_len = len;
+
+    // Finalize encryption
+    if (EVP_EncryptFinal_ex(ctx, ciphertext + len, &len) != 1) {
+        perror("EVP_EncryptFinal_ex failed");
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    ciphertext_len += len;
+
+    // Free the context
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
 
 int main (int argc, char *argv[]) {
+
     // Program name (./client), Port, IP, File-to-Send
     if (argc != 4) {
         printf("Enter the following parameters: %s <server_ip> <port> <file-to-send>\n", argv[0]);
@@ -45,6 +91,12 @@ int main (int argc, char *argv[]) {
     }
     printf("Connected to server at %s:%d\n", server_ip, client_port);
 
+    unsigned char aes_key[AES_KEY_SIZE] = "thisisaverysecurekey1234567890";
+    unsigned char iv[AES_BLOCK_SIZE];
+    RAND_bytes(iv, AES_BLOCK_SIZE);
+
+    send(client_socket, iv, AES_BLOCK_SIZE, 0);
+
     // Open file to send
     FILE *file = fopen(file_to_send, "rb");
     if (!file) {
@@ -52,20 +104,26 @@ int main (int argc, char *argv[]) {
         close(client_socket);
         return EXIT_FAILURE;
     }
+        unsigned char buffer[AES_BLOCK_SIZE];
+    unsigned char encrypted_buffer[AES_BLOCK_SIZE + AES_BLOCK_SIZE]; // Encryption output buffer
+    int bytes_read;
 
-    char buffer[BUFFER_SIZE];
-    size_t bytes_read;
-    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
-        if (send(client_socket, buffer, bytes_read, 0) < 0) {
-            perror("File send failed.\n");
-            fclose(file);
-            close(client_socket);
-            return EXIT_FAILURE;
+    // Read and send file data
+    while ((bytes_read = fread(buffer, 1, AES_BLOCK_SIZE, file)) > 0) {
+        int encrypted_len = aes_encrypt(buffer, encrypted_buffer, bytes_read, aes_key, iv);
+        if (encrypted_len < 0) {
+            perror("Encryption failed");
+            break;
+        }
+
+        if (send(client_socket, encrypted_buffer, encrypted_len, 0) < 0) {
+            perror("File send failed");
+            break;
         }
     }
 
-
     fclose(file);
     close(client_socket);
+    printf("File sent successfully.\n");
     return EXIT_SUCCESS;
 }
--- src/server.c
+++ src/server.c
@@ -1,15 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h> // exit() and malloc()
-#include <string.h> // memset() and string ops
-#include <arpa/inet.h> // sockaddr_in and inet_pton()
-#include <unistd.h> // close()
+#include <stdlib.h>  // exit(), malloc()
+#include <string.h>  // memset(), string ops
+#include <arpa/inet.h>  // sockaddr_in, inet_pton()
+#include <unistd.h>  // close()
+#include <openssl/evp.h>
+#include <openssl/rand.h>
 
-#define BUFFER_SIZE 1024
+#define AES_KEY_SIZE 32
+#define AES_BLOCK_SIZE 16
 
-int main (int argc, char *argv[]) {
-    // Check for valid arguments
+// AES Decryption using EVP API
+int aes_decrypt(const unsigned char *ciphertext, unsigned char *plaintext, int ciphertext_len, unsigned char *key, unsigned char *iv) {
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int plaintext_len;
+
+    // Create decryption context
+    if (!(ctx = EVP_CIPHER_CTX_new())) {
+        perror("EVP_CIPHER_CTX_new failed");
+        return -1;
+    }
+
+    // Initialize AES-256-CBC decryption
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv) != 1) {
+        perror("EVP_DecryptInit_ex failed");
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    // Perform decryption
+    if (EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len) != 1) {
+        perror("EVP_DecryptUpdate failed");
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    plaintext_len = len;
+
+    // Finalize decryption
+    if (EVP_DecryptFinal_ex(ctx, plaintext + len, &len) != 1) {
+        perror("EVP_DecryptFinal_ex failed");
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    plaintext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext_len;
+}
+
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Enter the following parameters: %s <port> <output-file>\n", argv[0]); // output-file; where the sent file will save
+        printf("Usage: %s <port> <output-file>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -19,38 +60,38 @@ int main (int argc, char *argv[]) {
     // Create server socket
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
-        printf("Creating socket failed\n");
+        perror("Socket creation failed");
         return EXIT_FAILURE;
     }
 
-    // Set up server address structure
+    // Set up server address
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(server_port);
-    server_address.sin_addr.s_addr = INADDR_ANY; // Listen on any available interfaces
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind socket to the specified port
+    // Bind socket
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("Bind Failed");
+        perror("Bind failed");
         close(server_socket);
         return EXIT_FAILURE;
     }
 
     // Listen for connections
     if (listen(server_socket, 5) < 0) {
-        perror("Failed to listen for incoming connections.\n");
+        perror("Listen failed");
         close(server_socket);
         return EXIT_FAILURE;
     }
     printf("Server is listening on port %d...\n", server_port);
 
-    // Accept connection from client
+    // Accept connection
     struct sockaddr_in client_address;
     socklen_t client_address_len = sizeof(client_address);
     int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
     if (client_socket < 0) {
-        perror("Connection failed\n");
+        perror("Connection failed");
         close(server_socket);
         return EXIT_FAILURE;
     }
@@ -59,27 +100,48 @@ int main (int argc, char *argv[]) {
     // Open output file
     FILE *file = fopen(output_file, "wb");
     if (!file) {
-        perror("Failed to open file.\n");
+        perror("Failed to open file");
         close(client_socket);
         close(server_socket);
         return EXIT_FAILURE;
     }
 
-    // Receive data and write to file
-    char buffer[BUFFER_SIZE];
-    size_t received_bytes;
-    while ((received_bytes = recv(client_socket, buffer, BUFFER_SIZE, 0)) > 0) {
-        fwrite(buffer, 1, received_bytes, file);
+    // Receive IV from client
+    unsigned char iv[AES_BLOCK_SIZE];
+    if (recv(client_socket, iv, AES_BLOCK_SIZE, 0) != AES_BLOCK_SIZE) {
+        perror("Failed to receive IV");
+        fclose(file);
+        close(client_socket);
+        close(server_socket);
+        return EXIT_FAILURE;
+    }
+
+    // Initialize AES decryption
+    unsigned char aes_key[AES_KEY_SIZE] = "thisisaverysecurekey1234567890"; 
+    unsigned char buffer[AES_BLOCK_SIZE * 2];
+    unsigned char decrypted_buffer[AES_BLOCK_SIZE * 2];
+    int received_bytes;
+
+    // Receive and decrypt the file
+    while ((received_bytes = recv(client_socket, buffer, AES_BLOCK_SIZE, 0)) > 0) {
+        // Decrypt received data
+        int decrypted_len = aes_decrypt(buffer, decrypted_buffer, received_bytes, aes_key, iv);
+        if (decrypted_len < 0) {
+            perror("Decryption failed");
+            break;
+        }
+        // Write decrypted data to file
+        fwrite(decrypted_buffer, 1, received_bytes, file);
     }
 
     if (received_bytes < 0) {
-        perror("Data reception failed.\n");
+        perror("Data reception failed");
     } else {
-        printf("File received successfully.\n");
+        printf("File received and decrypted successfully.\n");
     }
 
     fclose(file);
     close(client_socket);
     close(server_socket);
     return EXIT_SUCCESS;
-} 
+}
